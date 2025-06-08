2753f5f1c684d5c024142e1418a6f743b0c21380
Vladislav Alkhimenko <polyar999@mail.ru>
2025-06-08 11:28:05 +0800

    Изменена логика и добавлен новый класс

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,7 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("11111");
+        System.out.println("3333");
+        System.out.println("2222");
     }
 }
---
+++ src/main/java/com/stepup/proj/cat.java
@@ -0,0 +1,8 @@
+package com.stepup.proj;
+
+public class cat {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
